f100ceac20a783829546bbcd6540328df42aeddf
tonisubi <toni.subirana@gmail.com>
2019-02-28 21:21:05 +0100

    Update get_slides.py

--- get_slides.py
+++ get_slides.py
@@ -14,16 +14,18 @@ def read_photo(infile):
             id = id+1
     return slides
 
+def combine_vertical_photos(photo1, photo2):
+    return (photo1[0], photo2[0]),photo1[2].union(photo2[2])
+
 def get_slides(infile):
     fotos = read_photo(infile)
 
     # Filtrar les fotos verticals
     fotos_verticals = fotos.copy()
-    filter(lambda x: x[1] == 'V', fotos_verticals)
     fotos_verticals = [x for x in fotos if x[1] == "V"]
 
     # Ordernar les fotos verticals segons el núm tags que tenen
-    fotos_verticals = sorted(fotos_verticals, key=lambda x:x[3] )
+    fotos_verticals = sorted(fotos_verticals, key=lambda x:x[3])
 
     # Si hi ha un núm senar de fotos verticals, eliminar la menys valuosa
     if len(fotos_verticals) % 2 > 0:
@@ -32,17 +34,20 @@ def get_slides(infile):
     # Combinar fotos verticals de 2 en 2 fent que la que té menys tags vagi amb la que en té més
     fotos_vert_comb = []
     while len(fotos_verticals) > 0:
-        fotos_vert_comb.append((fotos_verticals[0], fotos_verticals[len(fotos_verticals)-1]))
+        fotos_vert_comb.append(combine_vertical_photos(fotos_verticals[0], fotos_verticals[len(fotos_verticals)-1]))
         fotos_verticals.remove(fotos_verticals[0])
         fotos_verticals.remove(fotos_verticals[len(fotos_verticals)-1])
 
     # Extreure les fotos horitzontals
     fotos_horitzontals = fotos.copy()
-    filter(lambda x: x[1] == 'H', fotos_horitzontals)
     fotos_horitzontals = [x for x in fotos if x[1] == "H"]
 
+    fotos_horiz = []
+    for x in fotos_horitzontals:
+        fotos_horiz.append((x[0], x[2]))
+
     # Juntar totes les fotos en array de slides
-    slides = fotos_horitzontals + fotos_vert_comb
+    slides = fotos_horiz + fotos_vert_comb
     return(slides)
 
-get_slides(infile)
+print(get_slides(infile))
