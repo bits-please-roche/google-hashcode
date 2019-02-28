33b1ac55a06e6db4b562d0520e0f6a3cb00afcf6
tonisubi <toni.subirana@gmail.com>
2019-02-28 21:51:12 +0100

    Update get_slides.py

--- get_slides.py
+++ get_slides.py
@@ -15,7 +15,7 @@ def read_photo(infile):
     return slides
 
 def combine_vertical_photos(photo1, photo2):
-    return (photo1[0], photo2[0]),photo1[2].union(photo2[2])
+    return (photo1[0], photo2[0]), photo1[2].union(photo2[2])
 
 def get_slides(infile):
     fotos = read_photo(infile)
@@ -32,22 +32,23 @@ def get_slides(infile):
         fotos_verticals.remove(fotos_verticals[0])
 
     # Combinar fotos verticals de 2 en 2 fent que la que té menys tags vagi amb la que en té més
-    fotos_vert_comb = []
+    fotos_vert_comb = {}
     while len(fotos_verticals) > 0:
-        fotos_vert_comb.append(combine_vertical_photos(fotos_verticals[0], fotos_verticals[len(fotos_verticals)-1]))
+        key, value = combine_vertical_photos(fotos_verticals[0], fotos_verticals[len(fotos_verticals)-1])
+        fotos_vert_comb[key] = value
         fotos_verticals.remove(fotos_verticals[0])
         fotos_verticals.remove(fotos_verticals[len(fotos_verticals)-1])
-
+    
     # Extreure les fotos horitzontals
     fotos_horitzontals = fotos.copy()
     fotos_horitzontals = [x for x in fotos if x[1] == "H"]
 
-    fotos_horiz = []
+    fotos_horiz = {}
     for x in fotos_horitzontals:
-        fotos_horiz.append((x[0], x[2]))
+        fotos_horiz[x[0]] = x[2]
 
     # Juntar totes les fotos en array de slides
-    slides = fotos_horiz + fotos_vert_comb
+    slides = {**fotos_horiz, **fotos_vert_comb}
     return(slides)
 
 print(get_slides(infile))
